1000014bf304591cc518121d67b9fdd637d67ee6
mizrael <mizrael@yahoo.it>
2016-08-11 10:47:06 +0200

    first commit

---
+++ TextToSpeech.iOS/TextToSpeech.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using AVFoundation;
+
+namespace TextToSpeech
+{
+	public class TextToSpeech
+	{
+		private AVSpeechSynthesizer _synth;
+		private TextToSpeechConfig _config;
+
+		public TextToSpeech(TextToSpeechConfig config) {
+			SetConfig(config);
+			_synth = new AVSpeechSynthesizer();
+		}
+
+		public void Speak(string text)
+		{
+			var speechUtterance = new AVSpeechUtterance(text)
+			{
+				Rate = _config.Rate,
+				Voice = AVSpeechSynthesisVoice.FromLanguage(_config.Voice),
+				Volume = _config.Volume,
+				PitchMultiplier = _config.PitchMultiplier
+			};
+
+			_synth.SpeakUtterance(speechUtterance);
+		}
+
+		public void SetConfig(TextToSpeechConfig config)
+		{
+			if (null == config)
+				throw new ArgumentNullException(nameof(config));
+			_config = config;
+		}
+	}
+
+	public class TextToSpeechConfig
+	{
+		public TextToSpeechConfig()
+		{
+			this.Rate = AVSpeechUtterance.MaximumSpeechRate / 2;
+			this.Voice = "en-US";
+			this.Volume = 1f;
+			this.PitchMultiplier = 1f;
+		}
+
+		public float Rate { get; set; }
+		public string Voice { get; set; }
+		public float Volume { get; set; }
+		public float PitchMultiplier { get; set; }
+	}
+}
+
---
+++ TextToSpeech.iOS/ViewController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using UIKit;
+
+namespace TextToSpeech.iOS
+{
+	public partial class ViewController : UIViewController
+	{
+		private TextToSpeech _speaker;
+
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			_speaker = new TextToSpeech(new TextToSpeechConfig());
+
+			this.btnPlay.TouchUpInside += (sender, e) => {
+				var text = this.txtMessage.Text;
+				if (string.IsNullOrWhiteSpace(text))
+					return;
+				_speaker.Speak(text);
+			};
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
